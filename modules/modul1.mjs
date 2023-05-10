78fe5aa0dd0a87ea8077656e024b7e8470dab546
R()M)(N <111087019+tsrm1@users.noreply.github.com>
2023-05-10 22:21:09 +0200

    модули, условные конструкции, циклы

---
+++ modules/modul1.mjs
@@ -0,0 +1,7 @@
+// импортируем модуль sumNumbers из файла modulOne.mjs в текущей директории ./
+import sumNumbers from './modulOne.mjs'
+
+const res1 = sumNumbers(10, 2)
+console.log('Результат функции sumNumbers = ', res1)
+const res2 = sumNumbers(10, 20)
+console.log('Результат функции sumNumbers = ', res2)
